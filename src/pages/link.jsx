e23da984d4301ed0f372539fa9e55f729e5ee02c
PrateekJaiswal12 <140104968+PrateekJaiswal12@users.noreply.github.com>
2025-01-27 14:33:39 +0530

    Update link.jsx

--- src/pages/link.jsx
+++ src/pages/link.jsx
@@ -69,7 +69,7 @@ const Link = () => {
       <div className='flex flex-col gap-8 sm:flex-row justify-between'>
         <div className='flex flex-col items-start gap-8 rounded-lg sm:w-2/5'>
           <span className='text-6xl font-extrabold hover:underline cursor-pointer' >{url?.title}</span>
-          <a href={`https://teenie.vercel.app/${link}`} target='_blank' className='text-3xl sm:text-4xl text-blue-400 font-bold hover:underline'>
+          <a href={`${url?.original_url}`} target='_blank' className='text-3xl sm:text-4xl text-blue-400 font-bold hover:underline'>
             https://teenie.vercel.app/{link}
           </a>
 
